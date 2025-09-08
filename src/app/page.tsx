90b7eefde5f237063873871b18e69d1b7e94e383
williamEkim <dongyeon0308@gmail.com>
2025-09-08 23:40:30 +0900

    updated the type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useState } from "react"
-import { File, mockFiles } from "../lib/mock-data"
+import type { File } from "../lib/mock-data";
+import { mockFiles } from "../lib/mock-data"
 import { Folder, FileIcon, Upload, ChevronRight } from "lucide-react"
 import Link from "next/link"
 import { Button } from "~/components/ui/button"
