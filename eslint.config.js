4b1299989a0007b1dc3c05d12999cafd5815f32c
williamEkim <dongyeon0308@gmail.com>
2025-09-08 23:57:03 +0900

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,10 @@
 // eslint.config.js
-import typescript from "@typescript-eslint/eslint-plugin";
 import eslintPluginNext from "eslint-plugin-next";
 
 export default [
   {
     files: ["**/*.{js,ts,jsx,tsx}"],
-
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-
-    plugins: {
-      "@typescript-eslint": typescript,
-      "next": eslintPluginNext,
-    },
-
-    rules: {
-      // your rules here, e.g.:
-      "@typescript-eslint/no-unused-vars": "warn",
-      "no-console": "warn",
-    },
+    plugins: { next: eslintPluginNext },
+    rules: {},
   },
 ];
