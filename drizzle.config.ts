20a6ba4b1d80da09e6dd8a6150fb61ab039c53d2
williamEkim <dongyeon0308@gmail.com>
2025-09-09 19:29:56 +0900

    inital setup for db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ import { env } from "~/env";
 
 export default {
 	schema: "./src/server/db/schema.ts",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
+	dialect: "singlestore",
+	// dbCredentials: {
+	// 	url: env.DATABASE_URL,
+	// },
 	tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
